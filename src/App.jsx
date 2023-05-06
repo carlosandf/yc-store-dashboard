551ed7a7ab997dea1f39fe721a136ab2e1db4701
carlosandf <carlos.andres.ff2912@gmail.com>
2023-05-05 19:26:24 -0500

    Adding}

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
 import { useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { uploadFiles } from './services'
 
 function App() {
-  const [count, setCount] = useState(0)
   const inputFile = useRef()
+  const mainData = useRef()
+  const measuresAndDetails = useRef()
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const { target } = e
+  const handleClick = async () => {
+    const main = new FormData(mainData.current)
+    const mainDataEntries = Object.fromEntries(main)
 
-    //const files = Object.values(target)
-    console.log(target.files.files)
+    const measuresData = new FormData(measuresAndDetails.current)
+    const measuresDataEntries = Object.fromEntries(measuresData)
+
+    const files = Object.values(inputFile.current.files)
+    const [err, data] = await uploadFiles(files, 'watches', '123')
+    console.log({err, data})
+    console.log({mainDataEntries, measuresDataEntries, files })
   }
 
   return (
     <>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" name='brand' placeholder='Marca'/>
-        <input type="number" name='price' placeholder='Precio'/>
-        <input type="text" name='ref' placeholder='Referencia'/>
+      <div>
+        <form ref={mainData}>
+          <h3>Datos principales</h3>
+          <input type="text" name='brand' placeholder='Marca'/>
+          <input type="number" name='price' placeholder='Precio'/>
+          <input type="text" name='ref' placeholder='Referencia'/>
+        </form>
 
-        <input ref={inputFile} type="file" name='files' id="" multiple />
-        <button>enviar</button>
-      </form>
+        <form ref={measuresAndDetails} action="">
+        <h3>Mediadas</h3>
+          <input type="text" name='dial_diameter' placeholder='Diametro del dial' />
+          <input type="text" name='thickness' placeholder='Grosor de la carcasa' />
+          <input type="text" name='length' placeholder='Longitud' />
+          <input type="text" name='belt_width' placeholder='Ancho de la correa' />
+          <input type="text" name='weight' placeholder='Peso' />
+          <input type="text" name='belt_material' placeholder='Material de la correa' />
+          <input type="text" name='dial_material' placeholder='Material del dial' />
+
+          <h3>Detalles</h3>
+          <textarea name="details"></textarea>
+        </form>
+
+        <form action="">
+          <h3>Sube algunas imagenes</h3>
+          <input ref={inputFile} type="file" name='files' id="" multiple />
+          <button type='button' onClick={handleClick}>Enviar</button>
+        </form>
+      </div>
     </>
   )
 }
--- src/services/index.js
+++ src/services/index.js
@@ -9,23 +9,28 @@ const uploadFiles = async (files, filePath, ref) => {
   const images = []
   let error = null
 
-  for (const file of files) {
-    const {
-        data,
-        err 
-      } = await supabase
-        .storage
-        .from('images')
-        .upload(`${filePath}/${ref}_${images.length}.png`, file)
-    
-    if (err) {
-      error = err
-      console.log(error)
-    } else {
-      console.log(data)
+  files.forEach( async file => {
+    console.log({files})
+    const image = {...file}
+    console.log({image})
+    const { data, err } = await supabase
+      .storage
+      .from('images')
+      .upload(`/${filePath}/${ref}_00${images.length}.png`, file)
+
+      if (err) {
+        error = err
+        console.log(error)
+      } else {
+        console.log(data)
+        console.log(file)
       images.push(data?.path)
     }
-  }
+  });
+
+  // for (const file of files) {
+    
+  // }
 
   return [error, images]
 }
