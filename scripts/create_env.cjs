36c2af06f35873d0bfd9314f3bd9d3cb88acde31
carlosandf <carlos.andres.ff2912@gmail.com>
2023-05-10 13:09:00 -0500

    Fix

--- scripts/create_env.cjs
+++ scripts/create_env.cjs
@@ -1,11 +1,9 @@
 const fs = require('fs');
 
 const fileText =
-`
-VITE_SUPABASE_KEY=${process.env.VITE_SUPABASE_KEY}
+`VITE_SUPABASE_KEY=${process.env.VITE_SUPABASE_KEY}
 VITE_SUPABASE_URL=${process.env.VITE_SUPABASE_URL}
 VITE_IMAGE_URL=${process.env.VITE_IMAGE_URL}
-
 `
 
 fs.writeFileSync('./.env', fileText);
