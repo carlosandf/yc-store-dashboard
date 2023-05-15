d1175132c1b84e5fec79cf189e79c50e315cef82
carlosandf <carlos.andres.ff2912@gmail.com>
2023-05-14 22:23:04 -0500

    Fix urlGenerator

--- src/utils/urlGenerator.js
+++ src/utils/urlGenerator.js
@@ -1,5 +1,5 @@
 const urlGenerator = (ref, price, image) => {
-  const url = `https://api.whatsapp.com/send?phone=${import.meta.env.VITE_NUMBER_PHONE}&text=Hola%20quiero%20comprar%20%C3%A9ste%20producto%3A%0A*Ref: ${ref}%0A*Precio:* ${price}%0A*Imagen:* ${image}`
+  const url = `https://api.whatsapp.com/send?phone=${import.meta.env.VITE_NUMBER_PHONE}&text=Hola%20quiero%20comprar%20%C3%A9ste%20producto%3A%0A*Ref:* ${ref}%0A*Precio:* ${price}%0A*Imagen:* ${image}`
 
   console.log(url)
   return url
