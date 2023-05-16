34ad95a5e7cd2220ee92768328b7c2033f615523
carlosandf <carlos.andres.ff2912@gmail.com>
2023-05-15 19:15:18 -0500

    Update styles

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,15 +1,10 @@
-import { useContext } from 'react'
 import PropTypes from 'prop-types'
-import ProductInfo from './ProductInfo'
-import AppContext from '../context/AppContext'
 
 const Layout = ({ children }) => {
-  const { state } = useContext(AppContext)
   return (
-    <div>
+    <>
       {children}
-      {state.modal.isOpen && <ProductInfo />}
-    </div>
+    </>
   )
 }
 
